0d84d2944feae8ff0290e3a199f0ca65c5b3b7c5
Peach <scdzwyxst@gmail.com>
2017-03-19 20:52:39 +0800

    Enable parallax animation for the post list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import Parallax from './js/parallax';
+
 require('./sass/main.scss');
+
+/**
+ * Enable parallax for the post list
+ */
+Array.prototype.forEach.call(document.querySelectorAll('.post-item'), elm => new Parallax(elm, 50));
+
