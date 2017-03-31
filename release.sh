af2c2ab5f5e778189e9d53d1920e57fed088da8c
Peach <scdzwyxst@gmail.com>
2017-03-31 21:55:08 +0800

    Fix release script for version upgrade

--- release.sh
+++ release.sh
@@ -13,13 +13,12 @@ if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY = "" ]]; then
 
   echo "Creating publication files..."
 
+  git add -A
+  npm version $VERSION -m "Release $VERSION 🎉 🎉"
   npm run build
 
   zip -rq "./dist/meditation-${VERSION}.zip" LICENSE README.md package.json tag.hbs post.hbs layout.hbs index.hbs assets partials
   tar -czf "./dist/meditation-${VERSION}.tar.gz" LICENSE README.md package.json tag.hbs post.hbs layout.hbs index.hbs assets partials
-
-  git add -A
-  npm version $VERSION -m "Release $VERSION 🎉 🎉"
 
   git push origin refs/tags/v$VERSION
   git push
